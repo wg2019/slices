21d731d5104ebb4bfd9580095791ad188e28f0b0
小刚刚 <41517731+wg2019@users.noreply.github.com>
2020-12-15 22:49:55 +0800

    单元测试

--- rmd.go
+++ rmd.go
@@ -1,6 +1,8 @@
 package slices
 
-// RmDInt 整型切片去重
+import "fmt"
+
+// RmDInt 整型切片去重.
 func RmDInt(slice []int) []int {
 	intMap := make(map[int]bool)
 	newSlice := make([]int, 0)
@@ -13,7 +15,7 @@ func RmDInt(slice []int) []int {
 	return newSlice
 }
 
-// RmDString 字符串切片去重
+// RmDString 字符串切片去重.
 func RmDString(slice []string) []string {
 	intMap := make(map[string]bool)
 	newSlice := make([]string, 0)
@@ -22,6 +24,7 @@ func RmDString(slice []string) []string {
 			newSlice = append(newSlice, value)
 		}
 		intMap[value] = true
+		fmt.Printf("value: %+v", value)
 	}
-	return newSlice
+	return slice
 }
